f952da445323f4548c7dbe8c06635c7cbd03c45f
danyil <dakurb@ttu.ee>
2024-04-25 20:19:04 +0300

    migrations

---
+++ FoodFilter/App.DAL.EF/Migrations/20240425124427_RestaurantClaims.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestaurantClaims : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RestaurantClaim",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RestaurantId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClaimId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantClaim", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RestaurantClaim_Claims_ClaimId",
+                        column: x => x.ClaimId,
+                        principalTable: "Claims",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RestaurantClaim_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantClaim_ClaimId",
+                table: "RestaurantClaim",
+                column: "ClaimId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantClaim_RestaurantId",
+                table: "RestaurantClaim",
+                column: "RestaurantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RestaurantClaim");
+        }
+    }
+}
---
+++ FoodFilter/App.DAL.EF/Migrations/20240425130233_RestaurantClaimsTable.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestaurantClaimsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantClaim_Claims_ClaimId",
+                table: "RestaurantClaim");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantClaim_Restaurants_RestaurantId",
+                table: "RestaurantClaim");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RestaurantClaim",
+                table: "RestaurantClaim");
+
+            migrationBuilder.RenameTable(
+                name: "RestaurantClaim",
+                newName: "RestaurantClaims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantClaim_RestaurantId",
+                table: "RestaurantClaims",
+                newName: "IX_RestaurantClaims_RestaurantId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantClaim_ClaimId",
+                table: "RestaurantClaims",
+                newName: "IX_RestaurantClaims_ClaimId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RestaurantClaims",
+                table: "RestaurantClaims",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantClaims_Claims_ClaimId",
+                table: "RestaurantClaims",
+                column: "ClaimId",
+                principalTable: "Claims",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantClaims_Restaurants_RestaurantId",
+                table: "RestaurantClaims",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantClaims_Claims_ClaimId",
+                table: "RestaurantClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantClaims_Restaurants_RestaurantId",
+                table: "RestaurantClaims");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RestaurantClaims",
+                table: "RestaurantClaims");
+
+            migrationBuilder.RenameTable(
+                name: "RestaurantClaims",
+                newName: "RestaurantClaim");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantClaims_RestaurantId",
+                table: "RestaurantClaim",
+                newName: "IX_RestaurantClaim_RestaurantId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantClaims_ClaimId",
+                table: "RestaurantClaim",
+                newName: "IX_RestaurantClaim_ClaimId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RestaurantClaim",
+                table: "RestaurantClaim",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantClaim_Claims_ClaimId",
+                table: "RestaurantClaim",
+                column: "ClaimId",
+                principalTable: "Claims",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantClaim_Restaurants_RestaurantId",
+                table: "RestaurantClaim",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
