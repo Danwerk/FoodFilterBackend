fcf8e9b1f014994a880a2c1a4c63956d8de7ea8a
danyil <dakurb@ttu.ee>
2024-05-07 19:10:11 +0300

    missed migrations

---
+++ FoodFilter/App.DAL.EF/Migrations/20240507130843_IngredientAddedBy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class IngredientAddedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddedBy",
+                table: "Ingredients",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedBy",
+                table: "Ingredients");
+        }
+    }
+}
